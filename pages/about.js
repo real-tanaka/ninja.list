424689bc1d909c4a2f8e7de86e9d1924d2745725
Tanaka <tanaka@Tanakas-MacBook-Pro.local>
2021-12-28 19:30:28 +0200

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
